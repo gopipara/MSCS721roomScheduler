8729ac43d426f5dc628f0116aafbe017e159fe14
gopipara <gopi.para1@marist.edu>
2016-02-29 16:46:41 -0500

    surefirereport

---
+++ src/com/marist/mscs721/RoomSchedulerTest1.java
@@ -0,0 +1,16 @@
+package com.marist.mscs721;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoomSchedulerTest1 {
+	
+	
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
